129d68a40d2d655139412db8ed8ae0cb34a0a6be
jasonbrogrammer <jason.n.chang@gmail.com>
2013-06-14 14:29:43 -0700

    Initial Commit

---
+++ BootcampTodo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BootcampTodo
+//
+//  Created by Jason Chang on 6/14/13.
+//  Copyright (c) 2013 Jason Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BootcampTodo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  BootcampTodo
+//
+//  Created by Jason Chang on 6/14/13.
+//  Copyright (c) 2013 Jason Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
